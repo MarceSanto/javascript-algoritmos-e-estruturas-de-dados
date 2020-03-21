87a48a56c6273d538492cf67fd86a623f1992cac
Marcelo Santos <rocha.21@gmail.com>
2020-03-20 22:01:52 -0300

    introducao a pilhas

---
+++ 02-Pilhas/js/index.js
@@ -0,0 +1,32 @@
+// Pilhas
+function Stack() {
+  let items = []
+
+  this.push = function(element) {
+    // Adiciona um novo item a pilha
+  }
+
+  this.pop = function() {
+    // remove o item do topo da pilha
+  }
+
+  this.peek = function() {
+    // devolve o elemento que esta no topo da pilha
+  }
+
+  this.isEmpty = function() {
+    // informa se a pilha esta vazia
+  }
+
+  this.clear = function() {
+    // limpa a pilha
+  }
+
+  this.size = function() {
+    //informa o tamanho da pilha
+  }
+
+  this.print = function() {
+    //imprime a pilha no console
+  }
+}
